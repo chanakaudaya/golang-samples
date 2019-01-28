6abf7c35e11d7aff824c61e0f4c735fbd70b22ce
chanakaudaya <chanaka.leadership@gmail.com>
2019-01-28 23:17:20 +0530

    Add library implementation

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chanakaudaya/golang-samples/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
-	fmt.Println("Hello again!")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
